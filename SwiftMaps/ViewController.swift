24dd5fa7b39900333faeebe43fa441584e8899dd
Adam Crabtree <dude@noderiety.com>
2015-08-17 13:56:48 -0700

    Use placePicker instead of maps

--- SwiftMaps/ViewController.swift
+++ SwiftMaps/ViewController.swift
@@ -3,12 +3,23 @@ import GoogleMaps
 
 class ViewController: UIViewController {
 
+    var placePicker: GMSPlacePicker!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    let mapView = self.view as! GMSMapView
-    mapView.camera = GMSCameraPosition.cameraWithLatitude(-33.8600, longitude: 151.2094, zoom: 10)
   }
 
+    @IBAction func showPicker(sender: AnyObject) {
+        let center = CLLocationCoordinate2DMake(51.5108396, -0.0922251)
+        let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        
+        placePicker = GMSPlacePicker(config: config)
+        placePicker.pickPlaceWithCallback({ (place: GMSPlace?, error: NSError?) -> Void in
+            print(place)
+        })
+    }
 }
 
